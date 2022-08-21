72f1bff59ed38b194dc48791a4ec15ba7a1144b6
Shyam Babu Jayswal <jaisnj541@gmail.com>
2022-08-21 23:06:02 +0530

    Initial commit

---
+++ src/loopinlinkedlist.java
@@ -0,0 +1,28 @@
+public class loopinlinkedlist {
+    Node head;
+    class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
+            this.next=next;
+        }
+    }
+    public boolean hascycle(Node head){
+        if(head == null){
+            return false;
+        }
+        Node hare=head;
+        Node turtle=head;
+        while(hare != null || hare.next != null){
+            hare = hare.next.next;
+            turtle=turtle.next;
+            if(hare==turtle){
+
+                return true;
+            }
+
+        }
+        return false;
+    }
+}
